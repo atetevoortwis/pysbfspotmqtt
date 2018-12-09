3f09d4f9459b571b265ecda076e6fbf5e6422d2f
ALLSEAS0\AtV <atv@allseas.com>
2018-12-09 20:57:01 +0100

    Run in loop, logging

--- logsma.py
+++ logsma.py
@@ -3,6 +3,10 @@ import subprocess
 import os
 import paho.mqtt.publish as pub
 import re
+import logging
+import time
+logging.basicConfig(level=logging.INFO)
+interval=10
 mqtt = {'server': 'hass.local', 'port': 1883}
 sbfPath = '/usr/local/bin/sbfspot.3/SBFspot'
 sbfArgs = ['-v', '-nocsv' ,'-nosql','-finq']
@@ -18,32 +22,34 @@ patterns = {
     'Phase1AC': {'pattern': r'Phase 1 Pac :  (.*)kW - Uac: (.*)V - Iac:  (.*)A',
                 'tags': ['Power','Voltage','Current']},
 }
-if os.path.isfile(sbfPath):
-    print('Running SBFSPOT')
-    out = subprocess.Popen([sbfPath]+sbfArgs,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT)
-    stdout, stderr = out.communicate()
+while True:
+    logging.info("Running, sending to: %s:%s" % (mqtt['server'],mqtt['port']))
+    if os.path.isfile(sbfPath):
+        print('Running SBFSPOT')
+        out = subprocess.Popen([sbfPath]+sbfArgs,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+        stdout, stderr = out.communicate()
 
-    data=stdout.decode('utf8')
-else:
-    with open('data.txt') as f:
-        data = f.readlines()
-    mqtt = {'server': 'hass.local', 'port': 1883}
-    data = ''.join(data)
-
-for tag, pat in patterns.items():
-    if type(pat) is dict:
-        r =re.search(pat['pattern'],data)
-        tags = pat['tags']
+        data=stdout.decode('utf8')
     else:
-        r = re.search(pat, data)
-        tags = [tag]
-        tag=''
+        with open('data.txt') as f:
+            data = f.readlines()
+        mqtt = {'server': 'hass.local', 'port': 1883}
+        data = ''.join(data)
+
+    for tag, pat in patterns.items():
+        if type(pat) is dict:
+            r =re.search(pat['pattern'],data)
+            tags = pat['tags']
+        else:
+            r = re.search(pat, data)
+            tags = [tag]
+            tag=''
 
 
-    for v,t in zip(r.groups(),tags):
-        print('%s: %s' % (tag+t,float(v)))
-
-        pub.single('sma/%s/value' % (tag+t), float(v),keepalive=60,hostname= mqtt['server'], port=mqtt['port'])
+        for v,t in zip(r.groups(),tags):
+            logging.info("Sending: %s: %s" % (tag+t,float(v)))
+            pub.single('sma/%s/value' % (tag+t), float(v),keepalive=60,hostname= mqtt['server'], port=mqtt['port'])
+    time.sleep(interval)
 
