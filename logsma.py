e6db78dd1a8d9f019d35b71dd3676c1dda087eca
ALLSEAS0\AtV <atv@allseas.com>
2018-12-10 07:19:21 +0100

    Changed logging, exeption handling

--- logsma.py
+++ logsma.py
@@ -3,9 +3,16 @@ import subprocess
 import os
 import paho.mqtt.publish as pub
 import re
-import logging
+import logging, logging.handlers
 import time
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+try:
+    handler = logging.handlers.SysLogHandler(address = '/dev/log')
+    log.addHandler(handler)
+except Exception as e:
+    log.exception(e)
+
 interval=10
 mqtt = {'server': 'hass.local', 'port': 1883}
 sbfPath = '/usr/local/bin/sbfspot.3/SBFspot'
@@ -23,33 +30,37 @@ patterns = {
                 'tags': ['Power','Voltage','Current']},
 }
 while True:
-    logging.info("Running, sending to: %s:%s" % (mqtt['server'],mqtt['port']))
-    if os.path.isfile(sbfPath):
-        print('Running SBFSPOT')
-        out = subprocess.Popen([sbfPath]+sbfArgs,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT)
-        stdout, stderr = out.communicate()
+    try:
+        log.info("Running, sending to: %s:%s" % (mqtt['server'],mqtt['port']))
+        if os.path.isfile(sbfPath):
+            log('Running SBFSPOT')
+            out = subprocess.Popen([sbfPath]+sbfArgs,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+            stdout, stderr = out.communicate()
 
-        data=stdout.decode('utf8')
-    else:
-        with open('data.txt') as f:
-            data = f.readlines()
-        mqtt = {'server': 'hass.local', 'port': 1883}
-        data = ''.join(data)
-
-    for tag, pat in patterns.items():
-        if type(pat) is dict:
-            r =re.search(pat['pattern'],data)
-            tags = pat['tags']
+            data=stdout.decode('utf8')
         else:
-            r = re.search(pat, data)
-            tags = [tag]
-            tag=''
+            log.info('Running with test data')
+            with open('data.txt') as f:
+                data = f.readlines()
+            mqtt = {'server': 'hass.local', 'port': 1883}
+            data = ''.join(data)
+
+        for tag, pat in patterns.items():
+            if type(pat) is dict:
+                r =re.search(pat['pattern'],data)
+                tags = pat['tags']
+            else:
+                r = re.search(pat, data)
+                tags = [tag]
+                tag=''
 
 
-        for v,t in zip(r.groups(),tags):
-            logging.info("Sending: %s: %s" % (tag+t,float(v)))
-            pub.single('sma/%s/value' % (tag+t), float(v),keepalive=60,hostname= mqtt['server'], port=mqtt['port'])
+            for v,t in zip(r.groups(),tags):
+                log.info("Sending: %s: %s" % (tag+t,float(v)))
+                pub.single('sma/%s/value' % (tag+t), float(v),keepalive=60,hostname= mqtt['server'], port=mqtt['port'])
+    except Exception as e:
+        log.exception(e)
     time.sleep(interval)
 
