6d986a714bdc219411d4766e55b6771b2c20fac7
Ate te Voortwis <atetevoortwis@gmail.com>
2018-12-12 18:01:46 +0100

    Only check ETotal

--- logsma.py
+++ logsma.py
@@ -81,7 +81,7 @@ while True:
                 'v1': round(dataActual['EToday'] * 1000.),
                 'v2': round(dataActual['Phase1ACPower'] * 1000.)
             }
-            if dataActual['EToday']>0. and dataActual['Phase1ACPower']>0.:
+            if dataActual['ETotal']>0.:
                 #only send if no false zeros TODO: Fix this, how to check?
                 pvoutput.doPVOutputRequest(data)
     except Exception as e:
